029cb8d78746c06cf5247739ba6c8307662a9770
root <root@bitcoissue.com>
2018-09-06 16:41:33 +0900

    b

--- views/_layout/mobile/layout.php
+++ views/_layout/mobile/layout.php
@@ -120,16 +120,13 @@ var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
             <a href="<?php echo site_url(); ?>" title="<?php echo html_escape($this->cbconfig->item('site_title'));?>"><?php echo $this->cbconfig->item('site_logo'); ?>
             </a>
         </h1>
-        <?php 
-        if(element(0,element('active',$menu)))
-            echo '<span>';
-        else echo '<span style="visibility:hidden;">';
-         ?>
+        <span style="visibility:hidden;">
+        
          
             <label for="select">지역선택하기</label>
             <select id="region">
         <?php
-        
+        /*
         if (config_item('region_category')) {
             foreach (config_item('region_category') as $key => $value) {
 
@@ -137,6 +134,7 @@ var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
                 else echo '<option value='.current_url().'>'.$value.'</option>';
             }
         }
+        */
         ?>
             </select>
         </span>
@@ -213,6 +211,14 @@ $(document).on('click', '.viewmobileversion', function(){
 </script>
 
 <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-88829342-9', 'auto');
+    ga('send', 'pageview');
+    
     $(document).ready(function(){
 
         var select = $("#region");
